4c7711d5f1a1801a9c20bddfcf685d30d8f027b7
sathagesey33 <abdukadirmuhand@gmail.com>
2023-09-22 09:02:37 -0700

    Big O

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -9,14 +9,13 @@ void bubble_sort(int *array, size_t size)
 {
 	size_t i, kev;
 	int tempy;
-	int swappy;
-	
+
 	if (size < 2)
 		return;
 
 	for (i = 0; i < size - 1; i++)
 	{
-	swappy = 0;
+
 
 	for (kev = 0; kev < size - i - 1; kev++)
 	{
@@ -26,12 +25,10 @@ void bubble_sort(int *array, size_t size)
 	array[kev] = array[kev + 1];
 	array[kev + 1] = tempy;
 
-	swappy = 1;
 	print_array(array, size);
 	}
 	}
 
-	if (swappy == 0)
-	break;
+
 	}
 }
