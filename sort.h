14793ab79600dd65e2eb5d263fdb9877a58beacc
sathagesey33 <abdukadirmuhand@gmail.com>
2023-09-23 07:25:15 -0700

    selection

--- sort.h
+++ sort.h
@@ -24,4 +24,5 @@ void print_array(const int *array, size_t size);
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 listint_t *create_listint(const int *array, size_t size);
+void selection_sort(int *array, size_t size);
 #endif
