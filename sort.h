ed7a8448b8124a75001ea960daf96db4fb29d588
254guru <otienooluda@gmail.com>
2023-09-24 05:02:19 -0700

    proud betty

--- sort.h
+++ sort.h
@@ -13,13 +13,15 @@
  */
 typedef struct listint_s
 {
-    const int n;
-    struct listint_s *prev;
-    struct listint_s *next;
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
 } listint_t;
+
 /* print prototypes */
 void print_list(const listint_t *list);
 void print_array(const int *array, size_t size);
+
 /* custom function prototypes */
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
