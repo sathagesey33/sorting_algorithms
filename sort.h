3020d6aeef3c0b76e455a695a57296739e9d482c
sathagesey33 <abdukadirmuhand@gmail.com>
2023-09-24 06:06:19 -0700

    Shell sort

--- sort.h
+++ sort.h
@@ -29,6 +29,9 @@ listint_t *create_listint(const int *array, size_t size);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 int lomuto_partition(int *array, int low, int high, size_t size);
+void shell_sort(int *array, size_t size);
+
+
 /* helper function */
 void swap(int *a, int *b);
 void quicksort(int *array, int low, int high, size_t size);
