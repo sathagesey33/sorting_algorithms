e2729e2a527abd353f14dec743c5a25f9a324b8f
254guru <otienooluda@gmail.com>
2023-09-29 01:04:31 -0700

    proud betty

--- 103-merge_sort.c
+++ 103-merge_sort.c
@@ -8,41 +8,44 @@
  * @right: Pointer to the right subarray.
  * @right_size: Size of the right subarray.
  */
-void merge(int *array, int *left, size_t left_size, int *right, size_t right_size)
+void merge(int *array, int *left, size_t left_size,
+		int *right, size_t right_size)
 {
-    size_t i = 0, j = 0, k = 0;
-    int *merged_array = malloc((left_size + right_size) * sizeof(int));
+	size_t i = 0, j = 0, k = 0;
+	int *merged_array;
 
-    if (!merged_array)
-        return;
+	merged_array = malloc((left_size + right_size) * sizeof(int));
 
-    printf("Merging...\n");
-    printf("[left]: ");
-    print_array(left, left_size);
-    printf("[right]: ");
-    print_array(right, right_size);
+	if (!merged_array)
+	return;
 
-    while (i < left_size && j < right_size)
-    {
-        if (left[i] <= right[j])
-            merged_array[k++] = left[i++];
-        else
-            merged_array[k++] = right[j++];
-    }
+	printf("Merging...\n");
+	printf("[left]: ");
+	print_array(left, left_size);
+	printf("[right]: ");
+	print_array(right, right_size);
 
-    while (i < left_size)
-        merged_array[k++] = left[i++];
+	while (i < left_size && j < right_size)
+	{
+	if (left[i] <= right[j])
+	merged_array[k++] = left[i++];
+	else
+	merged_array[k++] = right[j++];
+	}
 
-    while (j < right_size)
-        merged_array[k++] = right[j++];
+	while (i < left_size)
+	merged_array[k++] = left[i++];
 
-    for (i = 0; i < left_size + right_size; i++)
-        array[i] = merged_array[i];
+	while (j < right_size)
+	merged_array[k++] = right[j++];
 
-    free(merged_array);
+	for (i = 0; i < left_size + right_size; i++)
+	array[i] = merged_array[i];
 
-    printf("[Done]: ");
-    print_array(array, left_size + right_size);
+	free(merged_array);
+
+	printf("[Done]: ");
+	print_array(array, left_size + right_size);
 }
 
 /**
@@ -52,17 +55,17 @@ void merge(int *array, int *left, size_t left_size, int *right, size_t right_siz
  */
 void merge_sort(int *array, size_t size)
 {
-        size_t middle = size / 2;
-    int *left = array;
-    int *right = array + middle;
-    size_t left_size = middle;
-    size_t right_size = size - middle;
+	size_t middle = size / 2;
+	int *left = array;
+	int *right = array + middle;
+	size_t left_size = middle;
+	size_t right_size = size - middle;
 
 	if (size < 2 || !array)
-        return;
+	return;
 
-    merge_sort(left, left_size);
-    merge_sort(right, right_size);
+	merge_sort(left, left_size);
+	merge_sort(right, right_size);
 
-    merge(array, left, left_size, right, right_size);
+	merge(array, left, left_size, right, right_size);
 }
